3c76af6286cbcd327894e366d3265f3709c422a1
Felipe Pontes <felipemfpontes@gmail.com>
2017-07-12 12:03:00 -0300

    Update QuickSort

--- algoritmos/SortAlgorithms/src/QuickSort.java
+++ algoritmos/SortAlgorithms/src/QuickSort.java
@@ -16,41 +16,51 @@ public class QuickSort implements SortAlgorithm {
 
     Random rand = new Random();
 
-    public Object[] _sort(Object v[], Comparator c, int esquerda, int direita) {
-        int esq = esquerda, dir = direita;
-        Object pivo = v[rand.nextInt(direita - esquerda + 1) + esquerda], aux;
+    public Object[] _sort(Object v[], Comparator c, int length) {
+        if (length > 1) {
+            Object[] lowers = new Object[length], equals = new Object[length], biggers = new Object[length];
+            Object pivo = v[rand.nextInt(length)];
+            int k = 0, kl = 0, ke = 0, kb = 0;
 
-        while (esq <= dir) {
-            while (c.compare(v[esq], pivo) < 0) {
-                esq++;
+            while (k < length) {
+                if (c.compare(pivo, v[k]) == 0) {
+                    equals[ke++] = v[k++];
+                } else if (c.compare(pivo, v[k]) > 0) {
+                    lowers[kl++] = v[k++];
+                } else {
+                    biggers[kb++] = v[k++];
+                }
             }
-            while (c.compare(v[dir], pivo) > 0) {
-                dir--;
-            }
-            if (esq <= dir) {
-                aux = v[esq];
-                v[esq] = v[dir];
-                v[dir] = aux;
-                esq++;
-                dir--;
-            }
-        }
 
-        if (dir > esquerda) {
-            _sort(v, c, esquerda, dir);
-        }
-        if (esq < direita) {
-            _sort(v, c, esq, direita);
+            lowers = _sort(lowers, c, kl);
+            biggers = _sort(biggers, c, kb);
+
+            return merge(lowers, kl, equals, ke, biggers, kb);
         }
 
         return v;
+    }
+
+    private Object[] merge(Object[] l, int ll, Object[] e, int el, Object[] b, int bl) {
+        int k = 0;
+        Object[] s = new Object[ll + el + bl];
+        for (int i = 0; i < ll; i++) {
+            s[k++] = l[i];
+        }
+        for (int i = 0; i < el; i++) {
+            s[k++] = e[i];
+        }
+        for (int i = 0; i < bl; i++) {
+            s[k++] = b[i];
+        }
+        return s;
     }
 
     @Override
     public Object[] sort(Object[] collection, Comparator comparator) {
         Object copy[] = new Object[collection.length];
         System.arraycopy(collection, 0, copy, 0, collection.length);
-        return _sort(copy, comparator, 0, copy.length - 1);
+        return _sort(copy, comparator, copy.length);
     }
 
 }
